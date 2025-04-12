1dee19e6182f4a0e2ad64a70376c627da05599da
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:24:07 +0700

    Update TimMax.c

--- TimMax.c
+++ TimMax.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "KhoiTaoCay.c"
+#include "TaoNode.c"
+#include "ThemNodeVaoCay.c"
+#include "TimMin.c"
+#include "ChieuCaoCay.c"
+#include "DiTimNodeTheMang.c"
+#include "XoaNode.c"
+#include "Menu.c"
 int TimMax(TREE t)
 {
     if (t == NULL) return -1; // cây rỗng
