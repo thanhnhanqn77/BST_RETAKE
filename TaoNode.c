668283fefcac7c73e4648a6b7219fe849f062cbb
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:29:36 +0700

    Update TaoNode.c

--- TaoNode.c
+++ TaoNode.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "KhoiTaoCay.c"
-#include "ThemNodeVaoCay.c"
-#include "TimMax.c"
-#include "TimMin.c"
-#include "ChieuCaoCay.c"
-#include "DiTimNodeTheMang.c"
-#include "XoaNode.c"
-#include "Menu.c"
+
 NODE* TaoNode(int x)
 {
     NODE* p = (NODE*)malloc(sizeof(NODE));
