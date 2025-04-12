0eb718e70331a88905edc9390dfd9929fa080d7d
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:29:57 +0700

    Update KhoiTaoCay.c

--- KhoiTaoCay.c
+++ KhoiTaoCay.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "TaoNode.c"
-#include "ThemNodeVaoCay.c"
-#include "TimMax.c"
-#include "TimMin.c"
-#include "ChieuCaoCay.c"
-#include "DiTimNodeTheMang.c"
-#include "XoaNode.c"
-#include "Menu.c"
+
 void KhoiTaoCay(TREE* t)
 {
     *t = NULL;
