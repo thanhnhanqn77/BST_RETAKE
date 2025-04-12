f7c9376eec91ae69a7e37157d33a61d02b408e21
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:24:20 +0700

    Update TimMin.c

--- TimMin.c
+++ TimMin.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "KhoiTaoCay.c"
+#include "TaoNode.c"
+#include "ThemNodeVaoCay.c"
+#include "TimMax.c"
+#include "ChieuCaoCay.c"
+#include "DiTimNodeTheMang.c"
+#include "XoaNode.c"
+#include "Menu.c"
 int TimMin(TREE t)
 {
     if (t == NULL) return -1;
