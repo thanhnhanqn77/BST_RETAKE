efad28c779ebd22d7b79fc5fa4f12456f00d0acc
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:30:08 +0700

    Update DiTimNodeTheMang.c

--- DiTimNodeTheMang.c
+++ DiTimNodeTheMang.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "KhoiTaoCay.c"
-#include "TaoNode.c"
-#include "ThemNodeVaoCay.c"
-#include "TimMax.c"
-#include "TimMin.c"
-#include "ChieuCaoCay.c"
-#include "XoaNode.c"
-#include "Menu.c"
+
 void DiTimNodeTheMang(NODE** x, NODE** y)
 {
     if ((*y)->pLeft != NULL)
