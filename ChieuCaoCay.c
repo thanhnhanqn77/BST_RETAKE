894cba56d225c35ce9ff7fa163ca5f7493909d58
thanhnhanqn77 <hnhan5355@gmail.com>
2025-04-12 10:30:17 +0700

    Update ChieuCaoCay.c

--- ChieuCaoCay.c
+++ ChieuCaoCay.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "KhoiTaoCay.c"
-#include "TaoNode.c"
-#include "ThemNodeVaoCay.c"
-#include "TimMax.c"
-#include "TimMin.c"
-#include "DiTimNodeTheMang.c"
-#include "XoaNode.c"
-#include "Menu.c"
+
 int ChieuCaoCay(TREE t)
 {
     if (t == NULL)
